ed181d25a79781d115b9ed882ada6c8ede465610
ReiArthurjpg <arthurnexora@gmail.com>
2026-09-02 16:10:56 -0300

    Ordem do pessoal

--- init.sql
+++ init.sql
@@ -7,17 +7,30 @@ CREATE TABLE IF NOT EXISTS companies (
     stars INT DEFAULT 0
 );
 
--- Insert default data if table is empty
-INSERT INTO companies (name, stars)
-SELECT * FROM (
-    SELECT 'Brother''s Burguer' AS name, 15 AS stars UNION ALL
-    SELECT 'Torres Pizza', 15 UNION ALL
-    SELECT 'Ulin Lanches', 15 UNION ALL
-    SELECT 'D''Cakes', 15 UNION ALL
-    SELECT 'Divorari', 15 UNION ALL
-    SELECT 'Seu Bibi Pastelaria', 15 UNION ALL
-    SELECT 'Imperial Pizza', 15
-) AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM companies
-);
+-- Limpa a tabela para recarregar os dados fixos
+TRUNCATE TABLE companies;
+
+-- Insere os dados padrão (fixos)
+INSERT INTO companies (name, stars) VALUES
+    ('Brother''s 1', 0),
+    ('Brother''s 2', 0),
+    ('Chez Vous', 0),
+    ('D''Cakes 1', 0),
+    ('D''Cakes 2', 0),
+    ('Divorari', 0),
+    ('Dr. Pizza', 0),
+    ('Flex Burguer', 0),
+    ('Food Métricas', 0),
+    ('Gorlami 1', 0),
+    ('Gorlami 2', 0),
+    ('Imperial Pizza', 0),
+    ('Lupe Pizza', 0),
+    ('Moo Very', 0),
+    ('Pizza Nostra 1', 0),
+    ('Pizza Nostra 2', 0),
+    ('Pudinni 1', 0),
+    ('Pudinni 2', 0),
+    ('Seu Bibi Pastelaria 1', 0),
+    ('Seu Bibi Pastelaria 2', 0),
+    ('Torres Pizza', 0),
+    ('Ulin Lanches', 0);
